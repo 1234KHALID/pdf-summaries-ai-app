e457335c39572dc5851162fc5f1df01bce13bcaa
Mohd Khalid <mohammad@prodios.com>
2025-06-27 10:56:48 +0530

    create bg-gradoent and hero-section page & components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <div>
-      <h1>khalid</h1>
-      <Button variant="outline" size="lg">
-        Hello nextjs
-      </Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection/>
+      <HowItWorksSection/>
+      <PricingSection/>
+      <CTASection/> */}
     </div>
   );
 }
